e39b759c280bf8c8cd5787b22248d95c3e25b275
Florent-W <35995337+Florent-W@users.noreply.github.com>
2024-02-07 15:55:06 +0100

    Correction de bugs pour les tests roi

--- __tests__/roi.test.ts
+++ __tests__/roi.test.ts
@@ -11,39 +11,39 @@ describe('Roi', () => {
     echiquier.setPieceAt([4, 4], roi);
   });
 
-  it('doit pouvoir se déplacer d'une case vers le haut', () => {
+  it('doit pouvoir se déplacer d\'une case vers le haut', () => {
     expect(echiquier.movePiece([4, 4], [4, 5])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case vers le bas', () => {
+  it('doit pouvoir se déplacer d\'une case vers le bas', () => {
     expect(echiquier.movePiece([4, 4], [4, 3])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case vers la gauche', () => {
+  it('doit pouvoir se déplacer d\'une case vers la gauche', () => {
     expect(echiquier.movePiece([4, 4], [3, 4])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case vers la droite', () => {
+  it('doit pouvoir se déplacer d\'une case vers la droite', () => {
     expect(echiquier.movePiece([4, 4], [5, 4])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case en diagonale haut-droite', () => {
+  it('doit pouvoir se déplacer d\'une case en diagonale haut-droite', () => {
     expect(echiquier.movePiece([4, 4], [5, 5])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case en diagonale haut-gauche', () => {
+  it('doit pouvoir se déplacer d\'une case en diagonale haut-gauche', () => {
     expect(echiquier.movePiece([4, 4], [3, 5])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case en diagonale bas-droite', () => {
+  it('doit pouvoir se déplacer d\'une case en diagonale bas-droite', () => {
     expect(echiquier.movePiece([4, 4], [5, 3])).toBeTruthy();
   });
 
-  it('doit pouvoir se déplacer d'une case en diagonale bas-gauche', () => {
+  it('doit pouvoir se déplacer d`\'une case en diagonale bas-gauche', () => {
     expect(echiquier.movePiece([4, 4], [3, 3])).toBeTruthy();
   });
 
-  it('ne doit pas pouvoir se déplacer de plus d'une case dans n'importe quelle direction', () => {
+  it('ne doit pas pouvoir se déplacer de plus d\'une case dans n\'importe quelle direction', () => {
     expect(echiquier.movePiece([4, 4], [4, 6])).toBeFalsy(); // Trop loin vers le haut
     expect(echiquier.movePiece([4, 4], [6, 4])).toBeFalsy(); // Trop loin vers la droite
     expect(echiquier.movePiece([4, 4], [2, 4])).toBeFalsy(); // Trop loin vers la gauche
