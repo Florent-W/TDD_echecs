cbd389ef5dbf6c1bd0864502ef9604abce60a192
dantin-durand <durand.dantin.pro@gmail.com>
2024-02-07 15:16:01 +0100

    [func] add pion

---
+++ src/pion.ts
@@ -0,0 +1,18 @@
+import { Piece } from "./piece";
+
+export class Pion extends Piece {
+  public firstMove: boolean = true;
+
+  canMoveTo(position: [number, number]): boolean {
+    let dx = position[0] - this.position[0];
+    let dy = position[1] - this.position[1];
+    let direction = this.color === "Blanc" ? 1 : -1;
+
+    if (this.firstMove) {
+      this.firstMove = false;
+      return (dx === 2 * direction || dx === direction) && dy === 0;
+    } else {
+      return dx === direction && dy === 0;
+    }
+  }
+}
